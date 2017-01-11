f0df379d9a3df2c1b5b29984cdb0458669931f80
A1 <internalmike@gmail.com>
2017-01-11 19:29:16 +0300

    script

--- src/NecroInit/Cli/Native.hs
+++ src/NecroInit/Cli/Native.hs
@@ -242,13 +242,36 @@ generateHairsPlugin game_dir plugin_name hs = do
   let
     plugin_header =
       [ T3Record (T3Mark SCPT) 0
-        [ T3ScriptField (T3Mark SCHD) (T3ScriptHeader "A1V1_Hairs_sc" 0 0 0 2 0)
-        , T3BinaryField (T3Mark SCDT) (decodeLenient $ C.pack "AQE=")
+        [ T3ScriptField (T3Mark SCHD) (T3ScriptHeader "A1V1_Hairs_sc" 1 1 0 96 11)
+        , T3BinaryField (T3Mark SCDT) (decodeLenient $ C.pack "BgECCSBzAQAgPCA1MAUBcwEACCBzAQAgMSArJAEJAQUBcwEAAiAwBgEBCSBYIBAgPT0gMSQBCQFlPxM4AAATOGwAAjwGAQEKIGwBACA+IDUwMCQBCQEROOgDAABkPgEB")
         , T3MultilineField (T3Mark SCTX)
           [ "Begin A1V1_Hairs_sc"
           , ""
+          , "short state"
+          , "long temp"
+          , ""
+          , "if ( state < 50 )"
+          , "  set state to state + 1"
+          , "  return"
+          , "endif"
+          , ""
+          , "set state to 0"
+          , ""
+          , "if ( MenuMode == 1 )"
+          , "  return"
+          , "endif"
+          , ""
+          , "setx temp to xGetCondition"
+          , ""
+          , "if (temp > 500 )"
+          , "  return"
+          , "endif"
+          , ""
+          , "xSetCondition 1000"
+          , ""
           , "End"
           ]
+        , T3MultiStringField (T3Mark SCVR) ["state", "temp"]
         ]
       ]
   let records = plugin_header ++ V.ifoldl makePlugin [] hs
