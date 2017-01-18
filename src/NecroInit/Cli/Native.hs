d1cd46d1f236b958a96c4b7b8a1446f5af284658
A1 <internalmike@gmail.com>
2017-01-18 23:49:07 +0300

    try use hash codes for names

--- src/NecroInit/Cli/Native.hs
+++ src/NecroInit/Cli/Native.hs
@@ -334,9 +334,10 @@ npcsSink game_dir hs =
       Just (T3Record (T3Mark NPC_) _ fields) -> do
         let name = fromMaybe "" $ getStringProperty (T3Mark NAME) fields
         let knam = fromMaybe "" $ getStringProperty (T3Mark KNAM) fields
-        let file_path = getFullPath game_dir $ npcsFiles ++ (getFileName $ T.unpack name)
+        let fnam = fromMaybe "" $ getStringProperty (T3Mark FNAM) fields
+        let file_path = getFullPath game_dir $ npcsFiles ++ show (getHashCode name)
         let hn = addHairs h knam
-        e <- liftIO $ tryIOError $ writeFile file_path $ getArmorID knam
+        e <- liftIO $ tryIOError $ writeFile file_path $ T.unpack fnam ++ ['\0'] ++ getArmorID knam
         case e of
           Left r -> return $ Left r
           Right _ -> go hn
@@ -350,6 +351,9 @@ getBodyPartID = take 31 . ("A1V1B" ++) . replace " " "" . replace "'" "" . repla
 
 getArmorID :: Text -> String
 getArmorID = take 31 . ("A1V1H" ++) . replace " " "" . replace "'" "" . replace "_" "" . T.unpack
+
+getHashCode :: Text -> Word32
+getHashCode = T.foldl (\h c -> h * 1664525 + fromIntegral (ord c) + 1013904223) 0
 
 getStringProperty :: T3Sign -> [T3Field] -> Maybe Text
 getStringProperty s fields =
